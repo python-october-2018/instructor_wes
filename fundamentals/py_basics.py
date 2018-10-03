077e4c54e8ff113191999dcddce9bc188827237a
Weston Harper <wes@tao.team>
2018-10-02 19:06:59 -0600

    updated first class classwork

---
+++ fundamentals/py_basics.py
@@ -0,0 +1,67 @@
+# Remember Things
+  # Variables
+  # Functions
+  # Databases
+# Math/Comparisons
+  # conditionals, if elif else
+  # arithmetic, + - / * %
+  # comparison, == != is not or and > < >= <=
+# Loops
+  # for
+  # while
+
+# Need to knows
+  # printing
+  # returning
+  # types
+  # commenting
+
+# Strongly Typed
+
+my_int = 10
+my_float = 12.0
+my_list = [1,2,3,4,5]
+my_string = "hello there"
+number_string = "10"
+my_dictionary = {
+  "key": "value"
+}
+
+my_dictionary['key'] = "other thing"
+my_dictionary['favorite_food'] = "pizza"
+print(my_dictionary['favorite_food'])
+print(my_dictionary)
+
+my_tuple = (1, 2, 3, 4, 5)
+my_bool = False
+
+# print(int(number_string) + my_int)
+
+# How do we check for type?
+# print(type(my_int))
+# print(isinstance(my_int, int))
+
+def my_func():
+  print("hello world")
+  return 5
+
+# print("hey there")
+
+# i = 0
+# while i <= 255:
+#   print(i)
+#   i += 1
+
+# for i in range(0, 256, 1):
+#   print(i)
+
+# print(my_list)
+# for num in my_list:
+#   print(num)
+
+# if 1 == 3:
+#   print("That's equal!")
+# elif 2 == 3:
+#   print("Woah")
+# else:
+#   print("Woof")
