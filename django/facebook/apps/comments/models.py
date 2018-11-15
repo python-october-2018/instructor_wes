d772f2124746b34ca34531c33a1682dcfff6976b
Weston Harper <wes@tao.team>
2018-11-15 08:47:06 -0700

    finished comments

--- django/facebook/apps/comments/models.py
+++ django/facebook/apps/comments/models.py
@@ -4,7 +4,25 @@ from ..users.models import User
 
 # Create your models here.
 class CommentManager(models.Manager):
-  pass
+  def validate(self, form):
+    errors = []
+
+    if len(form['content']) < 1:
+      errors.append('Must have some content')
+
+    return errors
+
+  def create_comment(self, form, user_id):
+    try:
+      user = User.objects.get(id=user_id)
+      post = Post.objects.get(id=form['post_id'])
+      self.create(
+        content = form['content'],
+        creator = user,
+        post = post
+      )
+    except:
+      print('SOMETHING WENT WRONG CREATING COMMENT')
 
 class Comment(models.Model):
   content = models.TextField()
@@ -12,3 +30,8 @@ class Comment(models.Model):
   post = models.ForeignKey(Post, related_name="comments")
   created_at = models.DateTimeField(auto_now_add=True)
   updated_at = models.DateTimeField(auto_now=True)
+  objects = CommentManager()
+
+  def full_name():
+    full_name = self.first_name + self.last_name
+    return full_name
--- django/facebook/apps/comments/views.py
+++ django/facebook/apps/comments/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
+from .models import Comment
 
 # Create your views here.
 def create(req):
-  Comment.objects.validate(req.POST, req.session['user_id'])
+  errors = Comment.objects.validate(req.POST)
+  if len(errors) > 0:
+    # flash messages
+    for error in errors:
+      messages.error(req, error)
+  else:
+    Comment.objects.create_comment(req.POST, req.session['user_id'])
   return redirect('/')
